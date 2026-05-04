d7790be521001892e0a8269ca00a4f4b98401dcd
Vinicius Bonfim <Viniciuscruz.bonfim@gmail.com>
2026-05-04 17:52:01 -0300

    Delete - Routes {OtherElements, Calendar & BasicTables}

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,6 @@ import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
 
-{/* Ui Elements */ }
-import Videos from "./pages/UiElements/Videos";
-import Images from "./pages/UiElements/Images";
-import Alerts from "./pages/UiElements/Alerts";
-import Badges from "./pages/UiElements/Badges";
-import Avatars from "./pages/UiElements/Avatars";
-import Buttons from "./pages/UiElements/Buttons";
-
-{/* Forms */ }
-import Calendar from "./pages/Calendar";
-import BasicTables from "./pages/Tables/BasicTables";
-import FormElements from "./pages/Forms/FormElements";
-
 {/* Dashboard */ }
 import Blank from "./pages/Blank";
 import AppLayout from "./layout/AppLayout";
@@ -46,7 +33,6 @@ export default function App() {
           
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<UserProfiles />} />
-          <Route path="/calendar" element={<Calendar />} />
           <Route path="/blank" element={<Blank />} />
 
           {/* Produtos */}
@@ -57,19 +43,7 @@ export default function App() {
           <Route path="/clientes" element={<ClientesList />} />
           <Route path="/clientes/add" element={<ClientesAdd />} />
 
-          {/* Forms */}
-          <Route path="/form-elements" element={<FormElements />} />
 
-          {/* Tables */}
-          <Route path="/basic-tables" element={<BasicTables />} />
-
-          {/* Ui Elements */}
-          <Route path="/alerts" element={<Alerts />} />
-          <Route path="/avatars" element={<Avatars />} />
-          <Route path="/badge" element={<Badges />} />
-          <Route path="/buttons" element={<Buttons />} />
-          <Route path="/images" element={<Images />} />
-          <Route path="/videos" element={<Videos />} />
         </Route>
 
         {/* Auth Routes (fora do AppLayout) */}
