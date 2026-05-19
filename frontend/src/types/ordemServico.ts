736efb38042f5888cd31b37346be026a909fec74
Vinicius Bonfim <Viniciuscruz.bonfim@gmail.com>
2026-05-19 06:38:18 -0300

    ADD - TIPOS OS

--- frontend/src/types/ordemServico.ts
+++ frontend/src/types/ordemServico.ts
@@ -1,3 +1,54 @@
-export interface ordemServico {
-    
+import { Veiculo } from "./veiculo";
+
+export type StatusOS =
+  | "Aberta"
+  | "Em andamento"
+  | "Aguardando Aprovação"
+  | "Aguardando peças"
+  | "Finalizada"
+  | "Entregue"
+  | "Cancelada";
+
+export interface ProdutoOS {
+  produtoId: number;
+  quantidade: number;
+  valorUnitario: number;
+  valorTotal: number;
+}
+
+export interface OrdemServico {
+  
+  // Cliente
+  clienteId: number;
+
+  // Veiculo
+  veiculo: Veiculo;
+
+  // Controle da OS
+  status: StatusOS;
+
+  // Produtos utilizados
+  produtos: ProdutoOS[];
+
+  // Diagnostico
+  diagnostico?: string;
+
+  // Mão de obra
+  descricaoServico?: string;
+
+  // Valores
+  valorProdutos: number;
+  valorServico: number;
+  valorTotal: number;
+
+  // Observações
+  observacoes?: string;
+
+  // Datas
+  dataAbertura?: string;
+  dataFinalizacao?: string;
+}
+
+export interface OrdemServicoAPI extends OrdemServico {
+  id: number;
 }
